70e81399afc55ba47ebec715d305b0b7d010095d
lucassecundes <lucassecundes@users.noreply.github.com>
2026-01-07 23:24:23 -0400

    Increment version code to 3 for updated app name build

--- app.config.ts
+++ app.config.ts
@@ -26,7 +26,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: "com.tagpro.app",
-    versionCode: 2,
+    versionCode: 3,
     adaptiveIcon: {
       foregroundImage: "./assets/images/adaptive-icon.png",
       backgroundColor: "#151515"
